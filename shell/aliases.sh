53a248ee4af73e1b954fa56b8e981b6119f8474e
kanghuynhba <kanghuynhba@gmail.com>
2025-06-04 00:21:17 +0700

    shell configure

---
+++ shell/aliases.sh
@@ -0,0 +1,10 @@
+set -o vi
+
+# cd alias
+alias tools='cd ~/tools'
+alias pyProject='cd ~/Workspace/projects/py-projects/'
+alias cppProject='cd ~/Workspace/projects/cpp-projects/'
+
+
+
+
---
+++ shell/functions.sh
@@ -0,0 +1,46 @@
+cppExe() {
+	echo "Executing programs at $(date)"
+	echo "Running $# cpp files which is: $@"
+
+	for file in "$@"; do
+		echo "Output of $file"
+		exeFile=$(echo "$file" | sed 's/.cpp/.exe/g')
+		g++ "$file" -o "./execution/"$exeFile""
+		./execution/"$exeFile"
+	done
+}
+
+# Open Google
+google() {
+	local query=$(echo $@ | sed 's/ /+/g') 
+	xdg-open https://www.google.com/search?q=$query 
+}
+
+listCourses() {
+	xdg-open https://space.bilibili.com/400647031/lists/389026?type=series
+}
+
+rmSwp() {
+	find . -name '*.swp' -exec rm {} \;
+	find . -name '*.swm' -exec rm {} \;
+	find . -name '*.swn' -exec rm {} \;
+	find . -name '*.swo' -exec rm {} \;
+	find . -name '*~' -exec rm {} \;
+}
+
+here() {
+    local loc
+    if [ "$#" -eq 1 ]; then
+        loc=$(realpath "$1")
+    else
+        loc=$(realpath ".")
+    fi
+    ln -sfn "${loc}" "$HOME/.shell.here"
+    echo "here -> $(readlink $HOME/.shell.here)"
+}
+
+there="$HOME/.shell.here"
+
+there() {
+    cd "$(readlink "${there}")"
+}
