d757859f1e40475bd5ead873bcb090337555e918
kanghuynhba <kanghuynhba@gmail.com>
2025-06-05 13:44:42 +0700

    configure colorscheme and syntax

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,17 +1,26 @@
 set -o vi
 
 # ls alias 
-alias ll='ls -la'
+alias ll="ls -la"
 
 # tldr alias
-alias forex='tldr'
+alias forex="tldr"
 
 # cd alias
-alias tools='cd ~/tools'
-alias tools='cd ~/tools'
-alias pyProject='cd ~/Workspace/projects/py-projects/'
-alias cppProject='cd ~/Workspace/projects/cpp-projects/'
-alias vimPlugins='cd ~/.vim/pack/vendor/start'
+projectPath="~/Workspace/projects"
+alias tools="cd ~/tools"
+alias tools="cd ~/tools"
+alias pyProject="cd $projectPath/py-projects/"
+alias lcProject="cd $projectPath/leetcode/"
+alias cppProject="cd $projectPath/cpp-projects/"
+alias vimPlugins="cd ~/.vim/pack/vendor/start"
 
+# vim alias
+shellPath="~/dotfiles/shell"
+alias aliasConfig="vim $shellPath/aliases.sh"
+alias funcConfig="vim $shellPath/functions.sh"
 
+# venv alias
+alias leetcode="source ~/venv/leetcode/bin/activate"
+alias icloud="source ~/venv/icloud/bin/activate"
 
